cf3aeb2f54720ab2c30c9a684dafed2eab1cd00e
John Lozano <john.lozano@ambonare.com>
2017-09-14 17:20:16 -0500

    revert server name

--- worker/src/Worker/Program.cs
+++ worker/src/Worker/Program.cs
@@ -16,7 +16,7 @@ namespace Worker
         {
             try
             {
-                var pgsql = OpenDbConnection("Server=voteapp10dev1_db;Username=postgres;");
+                var pgsql = OpenDbConnection("Server=db;Username=postgres;");
                 var redisConn = OpenRedisConnection("redis");
                 var redis = redisConn.GetDatabase();
 
